102eba8bfb0ce1e4c21e90c7ac1c0ea4dd785762
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-04-07 17:02:48 +0300

    Code style fixes

--- src/HeaderInterface.php
+++ src/HeaderInterface.php
@@ -8,21 +8,12 @@
  * @license   https://opensource.org/licenses/MIT MIT License
  * @link      https://vainyl.com
  */
-<<<<<<< HEAD
 declare(strict_types=1);
 
 namespace Vainyl\Http;
 
 use Vainyl\Core\NameableInterface;
 use Vainyl\Core\StringInterface;
-=======
-declare(strict_types = 1);
-
-namespace Vainyl\Http;
-
-use Vainyl\Core\Name\NameableInterface;
-use Vainyl\Core\String\StringInterface;
->>>>>>> Moved and refactored Header classes
 
 /**
  * Interface HeaderInterface
@@ -36,40 +27,24 @@ interface HeaderInterface extends NameableInterface, StringInterface
      *
      * @return HeaderInterface
      */
-<<<<<<< HEAD
     public function withName(string $name): HeaderInterface;
-=======
-    public function withName(string $name) : HeaderInterface;
->>>>>>> Moved and refactored Header classes
 
     /**
      * @param array $values
      *
      * @return HeaderInterface
      */
-<<<<<<< HEAD
     public function withValues(array $values): HeaderInterface;
-=======
-    public function withValues(array $values) : HeaderInterface;
->>>>>>> Moved and refactored Header classes
 
     /**
      * @return array
      */
-<<<<<<< HEAD
     public function getValues(): array;
-=======
-    public function getValues() : array;
->>>>>>> Moved and refactored Header classes
 
     /**
      * @param string $value
      *
      * @return HeaderInterface
      */
-<<<<<<< HEAD
     public function withAddedValue($value): HeaderInterface;
-=======
-    public function withAddedValue($value) : HeaderInterface;
->>>>>>> Moved and refactored Header classes
 }
