887534c5cd5c2b2f8abd08e795674f9e6ff1ca14
allflame <taras.p.gyrnik@gmail.com>
2017-04-12 00:41:39 +0300

    Added Response exceptions

---
+++ src/Exception/AbstractResponseException.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Http
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Http\Exception;
+
+use Psr\Http\Message\ResponseInterface;
+use Vainyl\Core\Exception\AbstractCoreException;
+
+/**
+ * Class AbstractResponseException
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+abstract class AbstractResponseException extends AbstractCoreException implements ResponseExceptionInterface
+{
+    private $response;
+
+    /**
+     * AbstractResponseException constructor.
+     *
+     * @param ResponseInterface $response
+     * @param string            $message
+     * @param int               $code
+     * @param \Exception|null   $previous
+     */
+    public function __construct(
+        ResponseInterface $response,
+        string $message,
+        int $code = 500,
+        \Exception $previous = null
+    ) {
+        $this->response = $response;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getResponse(): ResponseInterface
+    {
+        return $this->response;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toArray(): array
+    {
+        return array_merge(['response' => spl_object_hash($this->response)], parent::toArray());
+    }
+}
