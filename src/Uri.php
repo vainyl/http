74d48b751a560ed6746d33e957d18e5651d0cfee
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-04-12 15:21:01 +0300

    Added AbstractIdentifiable inheritance to Uri

--- src/Uri.php
+++ src/Uri.php
@@ -13,13 +13,14 @@ declare(strict_types=1);
 namespace Vainyl\Http;
 
 use Psr\Http\Message\UriInterface;
+use Vainyl\Core\AbstractIdentifiable;
 
 /**
  * Class Uri
  *
  * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
  */
-class Uri implements UriInterface
+class Uri extends AbstractIdentifiable implements UriInterface
 {
     const STANDARD_PORTS = ['http' => 80, 'https' => 443];
 
