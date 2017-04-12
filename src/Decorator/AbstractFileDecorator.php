ecfd2610dda40515901033cada4219542f5928cc
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-04-12 16:03:31 +0300

    Added missing abstract modificator

--- src/Decorator/AbstractFileDecorator.php
+++ src/Decorator/AbstractFileDecorator.php
@@ -19,7 +19,7 @@ use Psr\Http\Message\UploadedFileInterface;
  *
  * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
  */
-class AbstractFileDecorator implements UploadedFileInterface
+abstract class AbstractFileDecorator implements UploadedFileInterface
 {
     private $file;
 
